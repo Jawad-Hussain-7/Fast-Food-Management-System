6b442927b0228b0f13934f8659cdf70bd17ddf4d
Jawad-Hussain-7 <67473107+Jawad-Hussain-7@users.noreply.github.com>
2020-07-06 21:58:05 +0500

    Add files via upload

---
+++ src/Home Page/index.php
@@ -0,0 +1,27 @@
+<html>
+    <head>
+        <link type="text/css" rel="stylesheet" href="../template.css" />
+        <link type="text/css" rel="stylesheet" href="index.css" />
+    </head>
+    <body>
+        <script src="../template.js" type="text/javascript"></script>
+		<script>initHeader();</script>
+		<span id='type' style='display: none;'>
+			<?php echo $_COOKIE["usertype"] ?>
+		</span>
+		<script> initCompleteHeader(); </script>
+        <section>
+            <div id="s1">
+            </div>
+            <div class="row">
+                <div id="column1">
+                    <img src="home2.jpg" alt="Snow" style="width:100%">
+                </div>
+                <div id="column2">
+                    <img src="home3.jpg" alt="Snow" style="width:100%">
+                </div>
+            </div>
+        </section>
+        <script>initFooter();</script>
+    </body>
+</html>
